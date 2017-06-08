808e25432cdc49059c23eae4c8a719413e6bc959
Dave Dittrich <dittrich@u.washington.edu>
2017-06-07 20:39:11 -0700

    Remove unused prototype role

--- roles/rabbitmq-docker/run_rabbitmq_playbook.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-ansible-playbook -i ~/localhost_inv \
-    -e "rabbitmq_default_user=guest" \
-    -e "rabbitmq_default_user_pass=guest" \
-    -e "rabbitmq_admin_user=admin" \
-    -e "rabbitmq_admin_user_pass=admin" \
-    $GIT/ansible-playbooks/v2/rabbitmq-docker.yml -vvvv
