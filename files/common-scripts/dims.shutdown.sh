79b371e20ba8b54b4093d6669279acb0fae38b6a
Dave Dittrich <dittrich@u.washington.edu>
2017-08-13 19:50:11 -0700

    Bump version: 2.10.0 → 2.10.1

--- files/common-scripts/dims.shutdown.sh
+++ files/common-scripts/dims.shutdown.sh
@@ -33,7 +33,7 @@
 . $DIMS/bin/dims_functions.sh
 
 # Tracks with bumpversion
-DIMS_VERSION=2.10.0
+DIMS_VERSION=2.10.1
 
 # This script is designed to handle shutting down Vagrants before
 # the VM host, so DEPLOYMENT here applies to the Vagrants.
