fee0f4c3c1bd91f25bd0c9321c6b4358f2e3e0a7
Dave Dittrich <dittrich@u.washington.edu>
2017-08-14 16:23:15 -0700

    Bump version: 2.10.1 → 2.10.2

--- files/common-scripts/trigger.runner.sh
+++ files/common-scripts/trigger.runner.sh
@@ -36,7 +36,7 @@
 . $DIMS/bin/dims_functions.sh
 
 # Tracks with bumpversion
-DIMS_VERSION=2.10.1
+DIMS_VERSION=2.10.2
 
 TRIGGERDIR=${TRIGGERDIR:-$DIMS/triggers.d}
 TRIGGERSTATE=${TRIGGERSTATE:-''}
